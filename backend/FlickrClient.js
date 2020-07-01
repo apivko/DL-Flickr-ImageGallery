1414a7f1a9da69ca6e063a9487b2fff0fe2e06ce
Asher Pivko <apivko@gmail.com>
2020-07-01 09:40:09 +0300

    refactoring  FlickrClient

--- backend/FlickrClient.js
+++ backend/FlickrClient.js
@@ -1,23 +1,9 @@
 import config from './config';
 import fetch from 'node-fetch';
 
-
-
 const api_key = config.FLICKR_API_KEY;
 const flickrUrl = 'https://api.flickr.com/services/rest/';
 const format = 'json';
-
-const getUrlData = async (url) => {
-    try {
-        const data = await fetch(url);
-        if (data.fail)
-            throw data.message;
-        const json = await data.json();
-        return { data: json };
-    } catch (error) {
-        return { error: true, message: 'Error - Could not retrieve photos from server\n\\' + error }
-    }
-}
 
 /**
  * A singleton client to connect with Flickr
@@ -37,48 +23,68 @@ class FlickrClient {
         this.cacheSize = 100;
         this.cachedPhotos = {}; //empty the already cached photos 
     }
+    //Builds a flickr api url from a method and optional parameters 
+    buildUrl (method, params){
+        let url = flickrUrl + '?' + 'method=' + method  
+                            + '&api_key=' + '0fc0d3e04c56d2dd7f799a2e97ec6c7d'
+                            + '&gallery_id=' + this.galleryId
+                            + '&format=' + format
+                            + '&nojsoncallback=1';
+        if (params) {
+            Object.values(params).forEach((p) => {
+                url += p;
+            });
+        }
+        return url;
+    }
+    //Fetches data from flickr - async
+    async getUrlData(url) {
+        try {
+            const data = await fetch(url);
+            if (data.fail)
+                throw data.message;
+            const json = await data.json();
+            return { data: json };
+        } catch (error) {
+            return { error: true, message: 'Error - Could not retrieve photos from server\n\\' + error }
+        }
+    }
 
     //gets all photos from in a gallery
     //https://api.flickr.com/services/rest?method=flickr.galleries.getPhotos&api_key=0fc0d3e04c56d2dd7f799a2e97ec6c7d&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1
     getPhotos() {
-
-        const url = flickrUrl + '?' + 'method=flickr.galleries.getPhotos'
-                                    + '&api_key=' + '0fc0d3e04c56d2dd7f799a2e97ec6c7d'
-                                    + '&gallery_id=' + this.galleryId
-                                    + '&format=' + format
-                                    + '&nojsoncallback=1';
-        
-        return getUrlData(url);
+        const method = 'flickr.galleries.getPhotos';
+        const url = this.buildUrl(method);
+        return this.getUrlData(url);
     }
 
     //Retrieves a single photo info based on its id from flickr
     //https://api.flickr.com/services/rest?method=flickr.photos.getInfo&api_key=0fc0d3e04c56d2dd7f799a2e97ec6c7d&gallery_id=66911286-72157647277042064&photo_id=8432423659&format=json&nojsoncallback=1
     getPhotoInfo(photoId) {
-        const url = flickrUrl + '?' + 'method=flickr.photos.getInfo'
-                                    + '&api_key=' + '0fc0d3e04c56d2dd7f799a2e97ec6c7d'
-                                    + '&gallery_id=' + this.galleryId
-                                    + '&format=' + format
-                                    + '&photo_id=' + photoId
-                                    + '&nojsoncallback=1';
-        return getUrlData(url);
+        const method = 'flickr.photos.getInfo';
+        const params = {
+            photoId: '&photo_id=' + photoId
+        }
+        const url = this.buildUrl(method, params);
+
+        return this.getUrlData(url);
     }
 
 
     //Search flickrs photos in gallery based on a specified text 
     //https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=0fc0d3e04c56d2dd7f799a2e97ec6c7d&gallery_id=66911286-72157647277042064&text=dog&format=json&nojsoncallback=1
     searchPhotos(searchText,options) {
+        const method = 'flickr.photos.search';
         const page = options.page;
         const perPage = options.perPage;
-        const url = flickrUrl + '?' + 'method=flickr.photos.search'
-                                    + '&api_key=' + '0fc0d3e04c56d2dd7f799a2e97ec6c7d'
-                                    + '&gallery_id=' + this.galleryId
-                                    + '&format=' + format
-                                    + '&text=' + searchText
-                                    + '&nojsoncallback=1'
-                                    + '&page=' + page
-                                    + '&per_page=' + perPage;
 
-        return getUrlData(url);
+        const params = {
+            text: '&text=' + searchText,
+            page: '&page=' + page,
+            perPage: '&per_page=' + perPage
+        }
+        const url = this.buildUrl(method,params);
+        return this.getUrlData(url);
     }
 
     //Empty the already cached photos 
